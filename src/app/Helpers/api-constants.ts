8ee80f6f9143e96b093fe7611283c6ee841f8815
Ultra Instinct Om <omsapkal197@gmail.com>
2025-08-28 13:26:28 +0530

    Fix env files, fileReplacements, api constants

--- src/app/Helpers/api-constants.ts
+++ src/app/Helpers/api-constants.ts
@@ -1,6 +1,28 @@
+// import { environment } from '../../environments/environment';
+
+// const API = environment.apiBase.replace(/\/$/, '');   // trim trailing slash
+// const HOST = API.replace(/\/api$/, '');               // drop trailing /api
+
+// export const apiConstants = {
+//   // bases
+//   base_api: API,
+//   base_host: HOST,
+
+//   // auth & users
+//   login_user_path:        `${API}/User/LoginUser`,
+//   register_user_path:     `${API}/User/RegisterUser`,
+//   get_all_users:          `${API}/User/GetAllUsers`,
+
+//   // OTP / password
+//   login_step1_path:       `${API}/User/LoginStep1`,
+//   login_step2_path:       `${API}/User/LoginStep2`,
+//   forgot_password_path:   `${API}/User/ForgotPassword`,
+//   reset_password_path:    `${API}/User/ResetPassword`,
+// };
+
 import { environment } from '../../environments/environment';
 
-const API = environment.apiBase.replace(/\/$/, '');   // trim trailing slash
+const API  = environment.apiBase.replace(/\/$/, '');  // trim trailing slash
 const HOST = API.replace(/\/api$/, '');               // drop trailing /api
 
 export const apiConstants = {
@@ -9,13 +31,13 @@ export const apiConstants = {
   base_host: HOST,
 
   // auth & users
-  login_user_path:        `${API}/User/LoginUser`,
-  register_user_path:     `${API}/User/RegisterUser`,
-  get_all_users:          `${API}/User/GetAllUsers`,
+  login_user_path:      `${API}/User/LoginUser`,
+  register_user_path:   `${API}/User/RegisterUser`,
+  get_all_users:        `${API}/User/GetAllUsers`,
 
   // OTP / password
-  login_step1_path:       `${API}/User/LoginStep1`,
-  login_step2_path:       `${API}/User/LoginStep2`,
-  forgot_password_path:   `${API}/User/ForgotPassword`,
-  reset_password_path:    `${API}/User/ResetPassword`,
+  login_step1_path:     `${API}/User/LoginStep1`,
+  login_step2_path:     `${API}/User/LoginStep2`,
+  forgot_password_path: `${API}/User/ForgotPassword`,
+  reset_password_path:  `${API}/User/ResetPassword`,
 };
